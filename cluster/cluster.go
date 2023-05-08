10c51f00495267baacad0aa66d7f755c92eb24ba
fengqingkui <fengqingkui@163.com>
2023-05-08 15:32:46 +0800

    增加集群模块

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -1,1 +1,14 @@
 package cluster
+
+import (
+	"time"
+
+	"github.com/fengqk/mars-base/rpc"
+)
+
+const (
+	MAX_CLUSTER_NUM = int(rpc.SERVICE_NUM)
+	CALL_TIME_OUT   = 500 * time.Millisecond
+)
+
+type ()
