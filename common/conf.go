90bfaedfa40faa074950fc49e9368076b3c9e605
fengqingkui <fengqingkui@163.com>
2023-05-09 20:54:17 +0800

    修复数据类型

--- common/conf.go
+++ common/conf.go
@@ -5,22 +5,22 @@ import "github.com/fengqk/mars-base/rpc"
 type (
 	Server struct {
 		Ip   string `yaml:"ip"`
-		Port int    `yaml:"port"`
+		Port int32  `yaml:"port"`
 	}
 
 	Mysql struct {
 		Ip           string `yaml:"ip"`
-		Port         int    `yaml:"port"`
+		Port         int32  `yaml:"port"`
 		Name         string `yaml:"name"`
 		User         string `yaml:"user"`
 		Password     string `yaml:"password"`
-		MaxIdleConns int    `yaml:"maxIdleConns"`
-		MaxOpenConns int    `yaml:"maxOpenConns"`
+		MaxIdleConns int32  `yaml:"maxIdleConns"`
+		MaxOpenConns int32  `yaml:"maxOpenConns"`
 	}
 
 	Redis struct {
 		Ip       string `yaml:"ip"`
-		Port     int    `yaml:"port"`
+		Port     int32  `yaml:"port"`
 		Password string `yaml:"password"`
 	}
 
